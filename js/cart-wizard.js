98f27a2358af40c4c3aa1d88d7cbb9d8a4d6cf76
Meaghan Turner <mturner@northern.co>
2023-02-13 01:21:25 -0500

    Spacing file updates and qty selectors

--- js/cart-wizard.js
+++ js/cart-wizard.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
         var template = $('#meal-selection').html();
         var html = Mustache.render(template, data);
         $('.product-list').html(html);
-
+        
         for (var i = 0; i < data.products.length; i++) {
             var product = data.products[i];
         }
     });
-
+    
     // Cart
     var cartCount = $('.cart-wizard-main .cart-wizard-item').length;
     var checkoutBtn = $('#submit').prop("disabled", true).addClass("disabled");
@@ -22,29 +22,35 @@ $(document).ready(function () {
             alert("You have reached the maximum limit of 12 items.");
             return;
         }
-
+        
         var product = $(this).closest('.product-card-item');
         var title = product.find('h3').text();
         var img = product.find('img').attr('src');
         var mealId = title.toLowerCase().replace(/ /g, "-");
-
-        // Create a new HTML element to represent the product in the cart
-        var cartProduct = $('<li class="cart-wizard-item">');
-        cartProduct.append('' +
-            '<img src="' + img + '" />',
-            '<h3 class="title" data-meal-id="' + mealId + '">' + title + '</h3>',
-            '<button class="remove remove-item">Remove</button>', ' <div className="add-meal">' +
-            '<div className="quantity-selectors">' +
-            '<button className="remove-qty button">-</button>' +
-            '<p className="current-qty">1</p>' +
-            '<button className="add-qty">+</button>' +
-            '</div>' +
-            '</div>');
-
-        // Append the new element to the cart container
-        $('.cart-wizard-main').append(cartProduct);
+        
+        if (!$('.cart-wizard-item').find('[data-meal-id="' + mealId + '"]').length > 0) {
+            // Create a new HTML element to represent the product in the cart
+            var cartProduct = $('<li class="cart-wizard-item">');
+            cartProduct.append('' +
+                '<img src="' + img + '" />',
+                '<h3 class="title" data-meal-id="' + mealId + '">' + title + '</h3>',
+                '<button class="remove remove-item">Remove</button>',
+                '<div class="add-meal">' +
+                '<div class="quantity-selectors">' +
+                '<button class="remove-qty button">-</button>' +
+                '<p class="current-qty">1</p>' +
+                '<button class="add-qty">+</button>' +
+                '</div>' +
+                '</div>');
+            // Append the new element to the cart container
+            $('.cart-wizard-main').append(cartProduct);
+        } else {
+            var currentQty = $(this).siblings('.current-qty');
+            $(currentQty).text(parseInt($(currentQty).text()) + 1);
+        }
+        
         cartCount++;
-
+        
         var remainingItems = $('.remaining-items');
         if (cartCount <= 11) {
             remainingItems.text('Add ' + (12 - cartCount) + ' more meal' + (cartCount === 11 ? '' : 's') + '!');
@@ -55,32 +61,32 @@ $(document).ready(function () {
             checkoutBtn.prop("disabled", false).removeClass("disabled");
         }
     });
-
+    
     $(document).on('click', '.remove-qty', function () {
         var productId = $(this).closest('.product-card-item').attr('data-meal-id');
         var cartItem = $('.cart-wizard-main .cart-item-title:contains("' + productId + '")').closest('.cart-item');
         cartItem.remove();
     })
-
+    
     // Remove Single Item
     $(document).on('click', '.remove-item', function () {
         if (confirm("Are you sure you want to remove this item?")) {
             $(this).parent().remove();
         }
     });
-
+    
     // Remove All Items
     var cart = $(".cart-wizard-main");
     var removeButton = $(".remove-all");
     var toggleRemoveButton = function () {
         removeButton.prop("disabled", !cart.children().length).toggleClass('disabled', !cart.children().length > 0);
     };
-
+    
     removeButton.click(function () {
         if (!cart.children().length > 0) {
             return;
         }
-
+        
         if (confirm("Are you sure you want to delete all meals?")) {
             cart.empty();
             cartCount.text(parseInt(cartCount.text()) - cart.children().length);
@@ -93,7 +99,5 @@ $(document).ready(function () {
 });
 
 // TODO: update cart value after remove/add button
-// TODO: items within cart
 // TODO: add/remove qty selectors add to cart section on each product item
-// TODO: The item should only be added once to the cart, shouldn't be added again if it exists already. The quantity value should update instead.
-// TODO: If they click on minus and its on 1 qty it should remove the item
+// TODO: If they click on minus and its on 1 qty it should remove the item from cart
